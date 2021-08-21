099846636a5a7e1de9e44fef3946fb11cbc4ddec
Marq Roldan <hcmlroldan@gmail.com>
2021-08-21 20:51:59 +0800

    #2 Sample usage

--- src/ui/components/Welcome.vue
+++ src/ui/components/Welcome.vue
@@ -3,16 +3,27 @@
     <div class="visual-bell">
       <span class="visual-bell__msg">{{ message }}</span>
     </div>
+    <Button @click="sampleButton">Hi!</Button>
   </div>
 </template>
 
 <script>
+import { Button } from "figma-plugin-ds-vue";
+
 export default {
+  components: {
+    Button,
+  },
+  methods: {
+    sampleButton() {
+      console.log("Hello!");
+    },
+  },
   data() {
     return {
-      message: "Welcome to your new Figma Plugin"
+      message: "Welcome to your new Figma Plugin",
     };
-  }
+  },
 };
 </script>
 
