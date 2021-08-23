4f3375101629a9a06fee65f6c0815b6b9d68cab4
Marq Roldan <hcmlroldan@gmail.com>
2021-08-24 01:30:34 +0800

    #1 Created LabelInputButton component

---
+++ src/ui/components/LabelInputButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Label v-if="label">{{ label }}</Label>
+    <Input
+      icon="key"
+      v-bind:placeholder="placeholder"
+      v-model="inputValue"
+      @input="$emit('input', $event)"
+    />
+    <Button @click="btnPress">{{ btnLabel }}</Button>
+  </div>
+</template>
+
+<script>
+import { Button, Label, Input } from "figma-plugin-ds-vue";
+
+export default {
+  components: {
+    Input,
+    Label,
+    Button,
+  },
+  props: {
+    label: String,
+    btnLabel: String,
+    placeholder: String,
+    onButtonPress: Function,
+  },
+  methods: {
+    btnPress() {
+      if (this.onButtonPress) {
+        this.onButtonPress(this.inputValue);
+      }
+    },
+  },
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+};
+</script>
