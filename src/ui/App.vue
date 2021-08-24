e8d5b2b459362e7bf86e67624f9a865170bde8f1
Marq Roldan <hcmlroldan@gmail.com>
2021-08-24 11:17:56 +0800

    #1 Removed debugs

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -2,48 +2,22 @@
   <div>
     <Welcome />
     <APIKey />
-    {{ loading ? "yes i am" : "not loading" }}
-    <Button @click="hmmm">Hi!</Button>
+    <GSheetsURL />
   </div>
 </template>
 
 <script>
 import APIKey from "./components/APIKey";
+import GSheetsURL from "./components/GSheetsURL";
 import Welcome from "./components/Welcome";
 import { Button } from "figma-plugin-ds-vue";
-import { obtainGsheetsData } from "./helpers/gsheets";
 
 export default {
-  data() {
-    return {
-      loading: false,
-    };
-  },
-
   components: {
     Welcome,
     Button,
     APIKey,
-  },
-  methods: {
-    hmmm() {
-      this.loading = true;
-      this.$store.dispatch("fetchGSheetsData", {
-        url: "https://docs.google.com/spreadsheets/d/17YXfsS3mB5p174hZmfh_bnzONsdxV_nN13q0qgTDtMg/edit?usp=sharing",
-        error: () => {
-          console.log("HUH???");
-          this.loading = false;
-        },
-        success: () => {
-          console.log("Success!");
-          this.loading = false;
-        },
-      });
-    },
-  },
-  mounted() {
-    console.log("HIII");
-    // Initialize the figma-ds components
+    GSheetsURL,
   },
 };
 </script>
