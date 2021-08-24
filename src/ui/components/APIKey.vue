5b79160bc8236c3ff5c8c4df3a67479e66e7a6fc
Marq Roldan <hcmlroldan@gmail.com>
2021-08-24 09:31:11 +0800

    #1 Updated APIKey.vue with logic and handling

--- src/ui/components/APIKey.vue
+++ src/ui/components/APIKey.vue
@@ -2,6 +2,7 @@
   <div>
     <LabelInputButton
       label="GSheets API Key"
+      btnLabel="Save"
       placeholder="Save your GoogleSheets API here"
       :onButtonPress="saveApiKey"
       v-model="inputModel"
@@ -13,39 +14,42 @@
 <script>
 import LabelInputButton from "./LabelInputButton.vue";
 
-const getStoredAPIKey = "getStoredAPIKey";
-
 export default {
   components: {
     LabelInputButton,
   },
   methods: {
     updateData(value) {
-      this.inputModel = value;
+      const finalValue = value || "";
+      this.inputModel = finalValue;
+      this.$store.commit("changeKey", finalValue);
     },
     updateAPIKey({ data }) {
-      if (data.pluginMessage?.type === getStoredAPIKey) {
-        this.inputModel = "testing";
-        console.log("hm api key", data);
+      if (
+        data.pluginMessage?.type === "API" &&
+        data.pluginMessage?.action === "get"
+      ) {
+        this.inputModel = data.pluginMessage?.value;
+        this.$store.commit("changeKey", data.pluginMessage?.value);
       }
     },
     saveApiKey(apiKey) {
-      if (!apiKey) {
-        console.log("No value given");
-      } else {
-        console.log("this shoudl be working");
-        parent.postMessage({ pluginMessage: { type: getStoredAPIKey } }, "*");
+      if (apiKey) {
+        parent.postMessage(
+          { pluginMessage: { type: "API", action: "store", value: apiKey } },
+          "*"
+        );
       }
     },
   },
   data() {
     return {
       inputModel: "", // String || Number
-      message: "Welcome to your new Figma Plugin",
     };
   },
   mounted() {
     window.addEventListener("message", this.updateAPIKey);
+    parent.postMessage({ pluginMessage: { type: "API", action: "get" } }, "*");
   },
   beforeDestroy() {
     window.removeEventListener("message", this.updateAPIKey);
