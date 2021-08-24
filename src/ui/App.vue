5e7ea6760acff1b9f4ae0bb81e86837976f8ada1
Marq Roldan <hcmlroldan@gmail.com>
2021-08-24 16:21:10 +0800

    #7 Removed welcome

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Welcome />
     <APIKey />
     <GSheetsURL />
     <SheetsList />
@@ -11,12 +10,10 @@
 import APIKey from "./components/APIKey";
 import GSheetsURL from "./components/GSheetsURL";
 import SheetsList from "./components/SheetsList";
-import Welcome from "./components/Welcome";
 import { Button } from "figma-plugin-ds-vue";
 
 export default {
   components: {
-    Welcome,
     Button,
     APIKey,
     GSheetsURL,
