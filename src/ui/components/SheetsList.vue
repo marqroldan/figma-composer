19cd9b15cd9c48259682ca62b5820c763f588f4f
Marq Roldan <hcmlroldan@gmail.com>
2021-08-24 15:07:13 +0800

    #1 Sheets list components base

---
+++ src/ui/components/SheetsList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Label>Sheets List</Label>
+    <Select v-if="items.length" :items="items" :value="selectedSheet.key" />
+  </div>
+</template>
+<script>
+import { Select, Label } from "figma-plugin-ds-vue";
+
+export default {
+  components: { Select, Label },
+  computed: {
+    items() {
+      return this.$store.state.GSheets.sheets.map((item, index) => {
+        return {
+          label: item,
+          key: `item-${index + 1}`,
+        };
+      });
+    },
+  },
+  data() {
+    return {
+      selectedSheet: {},
+    };
+  },
+  watch: {
+    /*
+    items(to, from) {
+      if (
+        this.selectedSheet.key &&
+        !to.some((item) => item.key === this.selectedSheet.key)
+      ) {
+        this.selectedSheet = {};
+      } else {
+        if (!this.selectedSheet.key) {
+          this.selectedSheet = to[0] || {};
+        }
+      }
+    },
+    */
+  },
+};
+</script>
